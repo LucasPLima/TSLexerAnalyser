916e438136b779db480beafb7f11875054987a8b
Lucas Pereira <lucas.plima.ti@gmail.com>
2020-01-21 22:08:37 -0300

    Estrutura básica do analisador.

---
+++ src/lexicalAnalyser/Generator.java
@@ -0,0 +1,20 @@
+package lexicalAnalyser;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+public class Generator {
+
+    public static void main(String[] args) {
+
+        String rootPath = Paths.get("").toAbsolutePath(). toString();
+        String subPath = "/src/lexicalAnalyser/";
+
+        String file = rootPath + subPath + "language.lex";
+
+        File sourceCode = new File(file);
+
+        jflex.Main.generate(sourceCode);
+
+    }
+}
